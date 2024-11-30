8483da3f04bdde43c2f15913e180a15b929e7934
XboxSignOut <v.a.i.bhavi062024@gmail.com>
2024-11-30 16:10:18 +0530

    updated README.md

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -8,8 +8,6 @@ module.exports.registerUser = async (req, res, next) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  console.log(req.body);
-
   const { fullname, email, password } = req.body;
   const hashedPassword = await userModel.hashPassword(password);
 
